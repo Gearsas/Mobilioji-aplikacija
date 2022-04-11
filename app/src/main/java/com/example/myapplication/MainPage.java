ba75b7a2244800ebad69f0c1a0c3639d2b53e3e6
Gearsas <martynaskass@gmail.com>
2022-04-11 21:55:42 +0300

    main page is map now

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -1,50 +1,118 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainPage extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-    TextView username;
+public class MainPage extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGooglemap;
+    FloatingActionButton fab;
+    boolean check = false;
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
-        Button button = (Button) findViewById(R.id.button); // Pirmas muziejus
-        Button button9 = (Button) findViewById(R.id.button9); // Antras muziejus
-
-        username = findViewById(R.id.username);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMuseum("Lietuvos aviacijos muziejus");
-            }
-        });
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMuseum("Vytauto Didžiojo karo muziejus");
-            }
-        });
-
-        Intent intent = getIntent();
-
-        if (intent.getExtras() != null){
-            String passedUsername = intent.getStringExtra("data");
-            username.setText("Sveiki, "+ passedUsername);
+        checkMyPermision();
+        if (check) {
+            SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            supportMapFragment.getMapAsync((OnMapReadyCallback) this);
+            client = LocationServices.getFusedLocationProviderClient(this);
         }
+
+
     }
-    public void openMuseum(String museum){
-        Intent intent = new Intent(this, Museum.class ).putExtra("museum", museum);
-        startActivity(intent);
+    private void checkMyPermision() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                // Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
+                check = true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
     }
 
+    @Override
+    public void onMapReady(GoogleMap googlemap) {
+        mGooglemap = googlemap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGooglemap.setMyLocationEnabled(true);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        // LatLng latLng = new LatLng(location.getLatitude())
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    //float m = 16.00f;
+                    mGooglemap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+                    LatLng newlatlong = new LatLng(54.9038, 23.8924);
+                    markerOptions.position(newlatlong);
+                    markerOptions.title("pirmas muziejaus eksponatas");
+                    googlemap.addMarker(markerOptions);
+                }
+            }
+        });
+
+
+
+    }
 
 }
