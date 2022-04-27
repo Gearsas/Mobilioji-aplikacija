91f38d34b8a124f7ef783e62880c5f4bcde87dac
RutaOrd <79471966+RutaOrd@users.noreply.github.com>
2022-04-27 17:19:57 +0300

    login design

--- app/src/main/java/com/example/myapplication/landing_page.java
+++ app/src/main/java/com/example/myapplication/landing_page.java
@@ -15,30 +15,10 @@ public class landing_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
-        button10 = (Button) findViewById(R.id.button10);
-        button12 = (Button) findViewById(R.id.button12);
 
-        button10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openLogin();
-            }
-        });
-        button12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSignUp();
-            }
 
-        });
+
     }
 
-    public void  openLogin(){
-        Intent intent4 = new Intent(this, Login.class ).putExtra("login", getIntent().getStringExtra("login"));
-        startActivity(intent4);
-    }
-    public void  openSignUp(){
-        Intent intent5 = new Intent(this, SignUp.class ).putExtra("signup", getIntent().getStringExtra("signup"));
-        startActivity(intent5);
-    }
+
 }
